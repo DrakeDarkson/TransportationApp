a0adf2dd87d44dc1c8673bb8fbc0467a0dea3a0a
DrakeDarkson <brenodssan@gmail.com>
2024-03-19 02:48:43 -0300

    Comparison logic setup

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { GoogleMap, LoadScript, DirectionsRenderer } from '@react-google-maps/api';
+import compareTravels from "../../travelsComparison";
 import "./styles.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import backgroundImage from "../../images/icons/transport_car_icon.png";
+import carIcon from "../../images/icons/transport_car_icon.png";
+import uberIcon from "../../images/icons/uber_icon.png";
+import taxiIcon from "../../images/icons/99_icon.png";
+import unknownIcon from "../../images/icons/unknown_icon.png";
 
 function Home() {
   const [origin, setOrigin] = useState("");
@@ -11,6 +15,32 @@ function Home() {
   const [response, setResponse] = useState(null);
   const [estimatedTime, setEstimatedTime] = useState("");
   const [error, setError] = useState("");
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const appChosen = compareTravels();
+
+  const getAppIcon = () => {
+    switch (appChosen) {
+      case 'uber':
+        return uberIcon;
+      case '99':
+        return taxiIcon;
+      default:
+        return unknownIcon;
+    }
+  };
+
+  const handleStartButtonClick = () => {
+    switch (appChosen) {
+      case 'uber':
+        window.location.href = 'https://www.uber.com/';
+        break;
+      case '99':
+        window.location.href = 'https://99app.com/';
+        break;
+      default:
+        break;
+    }
+  };
 
   const mapContainerStyle = {
     width: '100%',
@@ -91,7 +121,7 @@ function Home() {
 
         <h2 className="menuTitle">Melhores Opções</h2>
         <button className="btn btn-primary">
-          <img src={backgroundImage} alt="car" className="icon"/>
+          <img src={carIcon} alt="car" className="icon"/>
         </button>
 
         <h2 className="menuTitle">Tempo Estimado / Preço</h2>
@@ -103,12 +133,13 @@ function Home() {
         />
 
         <h2 className="menuTitle">Aplicativo Selecionado</h2>
-        <button className="btn btn-secondary">
+        <img src={getAppIcon()} alt="app" className="appIcon" />
+        <button className="btn btn-secondary" onClick={handleStartButtonClick}>
           Iniciar ---
         </button>
       </div>
       <div className="map">
-        <LoadScript googleMapsApiKey="#API_KEY">
+        <LoadScript googleMapsApiKey={apiKey}>
           <GoogleMap
             mapContainerStyle={mapContainerStyle}
             center={center}
---
+++ frontend/src/travelsComparison.js
@@ -0,0 +1,15 @@
+const compareTravels = () => {
+    const uberPrice = 25;
+    const taxi99Price = 30;
+  
+    if (uberPrice < taxi99Price) {
+      return 'uber';
+    } else if (taxi99Price < uberPrice) {
+      return '99';
+    } else {
+      return 'uber';
+    }
+  };
+  
+  export default compareTravels;
+  
